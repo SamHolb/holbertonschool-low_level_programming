4bab45a9c74c9e36ea2de25a880209bdb23c94c7
SamHolb <5640@holbertonstudents.com>
2023-06-19 05:56:24 -0700

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/*
+ *printf: to print on stdout;
+ *return 0;
+ */
+
 int main(void)
 {	
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
