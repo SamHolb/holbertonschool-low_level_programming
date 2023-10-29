fea6f9e3b108aa4bb47862dd61af43546cdcac0f
SamHolb <5640@holbertonschool.com>
2023-10-29 12:54:49 -0700

    strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,29 +1,29 @@
 #include "main.h"
 
 /**
- * _strspn - Entry point
- * @s: input
- * @accept: input
- * Return: Always 0 (success)
+ * _strspn - Calculates the length of the initial segment of s that
+ * consists of only characters found in accept.
+ * @s: The input string.
+ * @accept: The string containing the characters to match.
+ * Return: The length of the initial segment of s consisting of characters in accept.
  */
-
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, n, value, check;
+    unsigned int i, n, value;
 
-	value = 0;
+    value = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-	check = 0;
+    for (i = 0; s[i] != '\0'; i++)
+    {
+    for (n = 0; accept[n] != '\0'; n++)
+    {
+    if (accept[n] == s[i])
+    {
+    value++;
+    break; /* You can add a break here to exit the inner loop once a match is found*/
+    }
+    }
+    }
 
-	for (n = 0; accept[n] != '\0'; n++)
-	{
-	if (accept[n] == s[i])
-	{
-	value++;
-	check = 1;
-	}
-	}
-	}
+    return value; 
 }
